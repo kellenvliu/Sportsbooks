876ad10d0dfbd5935a7c66e343a34088092bd979
kellenvliu <kellenvliu@gmail.com>
2021-01-27 23:19:21 -0800

    master.py iterates correctly

--- master.py
+++ master.py
@@ -5,26 +5,62 @@ import os
 import time
 
 #time interval in seconds for each query
-INTERVAL = 10
+INTERVAL = 20
 
-
+#db info
 cluster = MongoClient("mongodb+srv://kellen:testtest@cluster0.yvmkr.mongodb.net/sportsbook?retryWrites=true&w=majority")
 db = cluster['BetUS']
 collection = db['betus']
 
-results = collection.find()
-
-
-def collector(database):
-    os.system('cmd /c "scrapy crawl betus"')
-    
-    for result in database:
-        print(result)
-
+def collector(database): #clear, run, and tabulate collection
     collection.delete_many({})
+    os.system('cmd /c "scrapy crawl betus --nolog"')
+    results = database.find()
+    print(analyze(results))
+
+
+
+def analyze(matches):
+    currentmatches = [{
+        't1':'',
+        't2':'',
+        'line1':'',
+        'site1':'',
+        'line2':'',
+        'site2':''
+        }]
+        
+    for match in matches:
+        if match['t1'] > match['t2']:
+            match['t1'], match['t2'], match['line1'], match['line2'] = match['t2'], match['t1'], match['line2'], match['line1']
+        added = False
+        for idx, currentmatch in enumerate(currentmatches):
+            if currentmatch['t1'].lower() == match['t1'].lower(): #This is iffy since different sites have different spellings
+                print("got here")
+                if match['line1'] > currentmatch['line1']:
+                    currentmatches[idx]['line1'],currentmatches[idx]['site1'] = match['line1'],match['sitename']
+                if match['line2'] > currentmatch['line2']:
+                    currentmatches[idx]['line2'],currentmatches[idx]['site2'] = match['line2'],match['sitename']
+                added = True
+                break
+        if added == False:
+            print("also got here")
+            currentmatches.append({
+                't1':match['t1'],
+                't2':match['t2'],
+                'line1':match['line1'],
+                'site1':match['sitename'],
+                'line2':match['line2'],
+                'site2':match['sitename']
+                    })
+
+    print(currentmatches)
+    return currentmatches
+       
+
 
 while True:
-    collector(results)
+    collector(collection)
     time.sleep(INTERVAL)
 
 
